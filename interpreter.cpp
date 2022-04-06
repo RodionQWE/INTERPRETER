69c20d690d58001154810256d489b4883c4c1872
RodionQWE <90332130+RodionQWE@users.noreply.github.com>
2022-04-06 21:08:12 +0600

    3 этап

--- interpreter.cpp
+++ interpreter.cpp
@@ -22,14 +22,14 @@ public:
 	LEX_TYPE type;
 	Lexem(){}
 	int getLexType();
-	virtual int getType(){}
-	virtual int getPriority(){}
-//	virtual int getValue(int, int){}
-	virtual int getValue(){}
-	virtual int getEv(int, int){}
-	virtual void getEq(string, int){}
-	virtual void print(){}
-	virtual string getName(){}
+	virtual int getType(){return 0;}
+	virtual int getPriority(){return 0;}
+//	virtual int getValue(int, int){return 0;}
+	virtual int getValue(){return 0;}
+	virtual int getEv(int, int){return 0;}
+	virtual void getEq(string, int){return;}
+	virtual void print(){return;}
+	virtual string getName(){return 0;}
 	
 };
 
@@ -38,6 +38,13 @@ int Lexem::getLexType()
 	return type;
 }
 
+/*class NumVar: public Lexem
+{
+public:
+	int getValue();
+};
+
+*/
 class Var: public Lexem
 {
 	string name;
@@ -67,7 +74,7 @@ int Var::getValue()
 //	type = VAR;	
 //}
 
-
+	
 class Number: public Lexem
 {
 	int value ;
@@ -99,26 +106,51 @@ enum OPERATOR
         LBRACKET, RBRACKET,
         PLUS, MINUS,
         MULTIPLY, 
-	EQUALS
+	ASSIGN, EQ, NEQ,
+	LEQ, LT,
+	GEQ, GT,
+	OR, AND,
+	BITOR
 };
 
 int PRIORITY[] = {
         -1, -1,
-        0, 0,
-        1,
-	-2
+        9, 9,
+        10,
+	0, 6, 6,
+	7, 7,
+	7, 7,
+	1, 2,
+	3
+	
 };
 
-
+string OPERTEXT [] = 
+{
+"(" , ")",
+"==",
+":=" ,"!=", 
+"<=", ">=",
+"<<", ">>",
+"or" ,
+"and" ,
+"|" ,
+"^" ,
+"&" ,
+"<" , ">" ,
+"+" , "-" ,
+"*" , "/" , "%"
+};
 class Oper: public Lexem
 {
 	OPERATOR opertype;
 public:
-	Oper(char oper);
+	Oper(string oper);
 	int getPriority();
 	int getType();
 	int getEv (int left, int right);
-	void getEq(string, int);
+	void getEq (string name, int val);
+//	void getEq(string, int);
 //	int getValue (const string&, int);
 	void print();
 
@@ -137,7 +169,18 @@ void Oper::print()
 		case PLUS: cout << "+ "; break;
 		case MINUS: cout << "- "; break;
 		case MULTIPLY: cout << "* "; break;
-		case EQUALS: cout << "= "; break;
+		case ASSIGN: cout << ":= "; break;
+		case EQ: cout << "=="; break;
+		case NEQ: cout << "!="; break;
+		case LEQ: cout << "<="; break;
+		case GEQ: cout << ">="; break;
+		case LT: cout << "<"; break;
+		case GT: cout << ">"; break;
+		case OR: cout << "or"; break;
+		case AND: cout << "and"; break;
+		case BITOR: cout << "|"; break;
+		   
+
         }
 }
 
@@ -149,15 +192,30 @@ int Oper::getEv(int left = 1, int right = 1)
 		case PLUS: return left + right;
 		case MINUS: return left - right;
 		case MULTIPLY: return left * right;
+		case EQ: return left == right;
+		case NEQ: return !(left == right);
+		case LEQ: return left <= right;
+		case GEQ: return left >= right;
+		case LT: return left < right;
+		case GT: return left > right;
+		case OR: return left or right;
+		case AND: return left and right;
+		case BITOR: return left | right;
 	}
+	return 0;
 
 }
 
 void Oper::getEq(string name, int val)
 {
 	table[name] = val;
+	return ;
 }
-
+/*void Oper::getEq(string name, int val)
+{
+	table[name] = val;
+}
+*/
 /*int Oper::getValue(const string& left, int right)
 {
 //	table[left] = right;
@@ -166,21 +224,39 @@ void Oper::getEq(string name, int val)
 	
 }
 */
-Oper::Oper(char oper)
+Oper::Oper(string oper)
 {
 	type = OPER;
-	if (oper == '+')
+	if (oper == "+")
 		opertype = PLUS;
-	if (oper == '-')
+	if (oper == "-")
 		opertype = MINUS;
-	if (oper == '*')
+	if (oper == "*")
 		opertype = MULTIPLY;
-	if (oper == '(')
+	if (oper == "(")
 		opertype = LBRACKET;
-	if (oper == ')')
+	if (oper == ")")
 		opertype = RBRACKET;
-	if (oper == '=')
-		opertype = EQUALS;
+	if (oper == ":=")
+		opertype = ASSIGN;
+	if (oper == "==")
+		opertype = EQ;
+	if (oper == "!=")
+		opertype = NEQ;
+	if (oper == "<=")
+		opertype = LEQ;
+	if (oper == ">=")
+		opertype = GEQ;
+	if (oper == "<")
+		opertype = LT;
+	if (oper == ">")
+		opertype = GT;
+	if (oper == "or")
+		opertype = OR;
+	if (oper == "and")
+		opertype = AND;
+	if (oper == "|")
+		opertype = BITOR;
 }
 
 int Oper::getPriority()
@@ -190,12 +266,14 @@ int Oper::getPriority()
 
 
 
-vector <Lexem* > parseLexem(string codeline)
+/*vector <Lexem* > parseLexem(string codeline)
 {
 	vector <Lexem* > infix;
 	int number = 0;
 	int j, flag_n = 0;
-	string str;
+	int nOp = 0;
+//	string str;
+	string notNum = "";
 	for (int i = 0; i < codeline.size(); i++)
 	{
 		for (j = i; codeline[j] >= '0' && codeline[j] <= '9'; j++)
@@ -210,29 +288,108 @@ vector <Lexem* > parseLexem(string codeline)
 			i = j - 1;
 			number = 0;
 			flag_n = 0;
-		} else {
-			if (codeline[i] != ' ' && codeline[i] != '\t' && codeline[i] != '\n')
+		//	cout << "YEs" << endl;
+			continue;
+		}
+		if (codeline[i] != ' ' && codeline[i] != '\t' && codeline[i] != '\n' && flag_n == 0)
+		{
+			while (codeline[i] != ' ' && codeline[i] != '\t' && codeline[i] != '\n' && i < codeline.size())
 			{
-				if (codeline[i] == '+' || codeline[i] == '-' || codeline[i] == '*' || codeline[i] == '('
-					       	|| codeline[i] == ')' || codeline[i] == '=')
-				{
-					Oper *op = new Oper(codeline[i]);
-					infix.push_back(op);
-				}else
-				{
-					str = codeline[i];
-					Var *var = new Var(str);
-					infix.push_back(var);
-				}
+				notNum += codeline[i];
+				i++;
 
                         }
+			if (notNum == "+" || notNum == "-" || notNum == "*" || notNum == "(" || notNum == ")" 
+					|| notNum == "=" || notNum == ">" || notNum == "<" || notNum == ">=" || notNum == "<=" || notNum == "!=" 
+						|| notNum == "&" || notNum == "|" || notNum == "^" || notNum == ">>" || notNum == "<<" || notNum == "or" ||
+					       	notNum == "and")
+			{
+//				cout << " yes" << endl;
+				Oper *op = new Oper(notNum);
+				infix.push_back(op);
+			}
+			else
+			{
+/		}
+			nOp = 0;
+			notNum = "";
 
 		}
 
 	}
 	return infix;
 }
+*/
 
+vector <Lexem* > parseLexem(string codeline)
+{
+	vector <Lexem* > infix;
+	int number = 0;
+	int j, flag_n = 0, flag_op = 0;
+	int nOp = 0;
+//	string str;
+	string notNum = "";
+	for (int i = 0; i < codeline.size(); i++)
+	{
+		for (j = i; codeline[j] >= '0' && codeline[j] <= '9'; j++)
+		{
+			number = number * 10 + (codeline[j] - '0');
+			flag_n = 1;
+		}
+		if (flag_n == 1)
+		{
+			Number *num = new Number(number);
+			infix.push_back(num);
+			i = j - 1;
+			number = 0;
+			flag_n = 0;
+		//	cout << "YEs" << endl;
+			continue;
+		}
+	
+		int n = sizeof(OPERTEXT) / sizeof(string);
+		for ( int op = 0; op < n ; op ++)
+		{
+			string subcodeline = codeline.substr(i, OPERTEXT[op].size());
+			if(OPERTEXT [op] == subcodeline ) 
+			{	
+				infix.push_back(new Oper (OPERTEXT[op]));
+//				cout <<"  ! " << OPERTEXT[op] << endl;
+				i += OPERTEXT[op].size() - 1 ;
+				flag_op = 1;
+ 				break;
+			}
+		}
+		if (flag_op == 1)
+		{
+			flag_op = 0;
+			continue;	
+		}
+		if (codeline[i] != ' ' && codeline[i] != '\n')
+		{	
+//		cout << "YES" << endl;
+			while (((codeline[i] >= 'a' && codeline[i] <= 'z') || (codeline[i] >= 'A' && codeline[i] <= 'Z')) && i < codeline.size())
+			{
+				notNum += codeline[i];
+				i++;
+			}
+			if (notNum != "")
+			{
+				infix.push_back(new Var(notNum));
+//				cout << "!" << notNum << "!";
+				i--;
+			}	
+			notNum = "";
+			flag_op = 0;
+		}
+
+	
+
+	}
+	return infix;
+
+
+}
 vector<Lexem *> buildPostfix(vector<Lexem *> infix)
 {
         int i, j;
@@ -246,12 +403,13 @@ vector<Lexem *> buildPostfix(vector<Lexem *> infix)
                 }
                 if (infix[i]->getLexType() == OPER)
 		{
-                        if (infix[i]->getType() == LBRACKET)
+			Oper *oper = static_cast <Oper* >(infix[i]);
+                        if (oper->getType() == LBRACKET)
 			{
-                                opstack.push((Oper *)infix[i]);
+                                opstack.push((oper));
                         } else
 			{
-                          if (infix[i]->getType() == RBRACKET)
+                          if (oper->getType() == RBRACKET)
 				{
                                         for (j = opstack.size(); j > 0 && opstack.top()->getType() != LBRACKET; j--)
 					{
@@ -260,7 +418,7 @@ vector<Lexem *> buildPostfix(vector<Lexem *> infix)
                                         }
                                         opstack.pop();
                                 } else {
-                                        while (!opstack.empty() && opstack.top()->getPriority() > infix[i]->getPriority())
+                                        while (!opstack.empty() && opstack.top()->getPriority() >= oper->getPriority())
 					{
                                                 postfix.push_back(opstack.top());
                                                 opstack.pop();
@@ -282,34 +440,39 @@ int evaluatePostfix(vector<Lexem *> poliz)
 {
         int value, left, right;
 	string name;
-        stack<Lexem *> computationStack;
+        stack<Lexem*> computationStack;
         for (int i = 0; i < poliz.size(); i++)
 	{
                 if (poliz[i]->getLexType() == NUMBER || poliz[i]->getLexType() == VAR)
 		{
                         computationStack.push(poliz[i]);
                 }
-                if (poliz[i]->getLexType() == OPER && poliz[i]->getType() != EQUALS)
+                if (poliz[i]->getLexType() == OPER)
 		{
+			Oper *oper = static_cast <Oper* >(poliz[i]);
                         right = (computationStack.top())->getValue();
-                        computationStack.pop();
-                        left = (computationStack.top())->getValue();
-                        computationStack.pop();
-                        value = poliz[i]->getEv(left, right);
-			Number *val = new Number(value);
-                        computationStack.push(val);
-                }
-		if (poliz[i]->getLexType() == OPER && poliz[i]->getType() == EQUALS)
-		{
-			right = (computationStack.top())->getValue();
-                        computationStack.pop();
-			name = (computationStack.top())->getName();
-                        computationStack.pop();
-			poliz[i]->getEq(name, right);
-			Var *var = new Var(name);
-                        computationStack.push(var);
-		}
-        }
+                       	computationStack.pop();
+			if (poliz[i]->getType() != ASSIGN)
+			{
+        	       	        left = (computationStack.top())->getValue();
+	                        computationStack.pop();
+        	                value = poliz[i]->getEv(left, right);
+	               	        Number *val = new Number(value);
+                       		computationStack.push(val);
+
+			}	
+
+
+			if (poliz[i]->getType() == ASSIGN)
+			{
+				name = (computationStack.top())->getName();
+				poliz[i]->getEq(name, right);
+				Number *val = new Number(value);
+             		        computationStack.push(val);
+			}
+
+        	}
+	}
 	return computationStack.top()->getValue();
 }
 
@@ -323,7 +486,7 @@ int evaluatePostfix(vector<Lexem *> poliz)
 		{
                         computationStack.push(poliz[i]->getValue());
                 }
-                if (poliz[i]->getLexType() == OPER && poliz[i]->getType() != EQUALS)
+                if (poliz[i]->getLexType() == OPER)
 		{
                         right = computationStack.top();
                         computationStack.pop();
@@ -332,7 +495,6 @@ int evaluatePostfix(vector<Lexem *> poliz)
                         value = poliz[i]->getEv(left, right);
                         computationStack.push(value);
                 }
-                if (poliz[i]->getLexType() == OPER && poliz[i]->getType() == EQUALS)
         }
         return computationStack.top();
 }
@@ -350,16 +512,25 @@ int main()
 	vector<Lexem *> postfix;
 	int ans;
 
-	while (std::getline(std::cin, codeline))
+	while (getline(cin, codeline))
 	{
 		infix = parseLexem(codeline);
 		postfix = buildPostfix(infix);
-		for (int i = 0; i < postfix.size(); ++i)
+		for (int i = 0; i < postfix.size(); i++)
 		{
                         postfix[i]->print();
 			cout << endl;
-//			cout << postfix.size();
+  		}
+	
+/*	for (int i = 0; i < infix.size(); i++)
+		{
+                        infix[i]->print();
+			cout << endl;
                 }
+
+
+*/
+//			cout << infix.size();
 		ans = evaluatePostfix(postfix);
 		cout << "ans = " << ans << endl;
 
